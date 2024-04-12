a1aafe44c02589c90cd5f8edc81fd062e20ab95e
Lisbeth Purrucker <lisbethpurrucker@gmail.com>
2024-04-12 14:14:59 +0200

    add contact component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,12 @@
   import "../app.css";
   import Footer from '../components/Footer.svelte';
   import Header from '../components/Header.svelte';
+  import Contact from '../components/Contact.svelte';
 </script>
 
 <div class="px-6 py-12">
   <Header />
   <slot />
+  <Contact />
   <Footer />
 </div>
