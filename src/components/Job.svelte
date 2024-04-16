2d9abc24ea10ce0ed7725be60931cb70e5892c0b
Lisbeth Purrucker <lisbethpurrucker@gmail.com>
2024-04-16 17:01:22 +0200

    add job component

---
+++ src/components/Job.svelte
@@ -0,0 +1,73 @@
+<script>
+  let jobs = [
+    {
+      duration: "2022 - Present",
+      title: "Quouch - CTO & Fullstack Developer",
+      description: "Provide leadership in technical decision making. Designed, developed, shipped and now maintain production code for our web application which is now used by 5000+ users worldwide.",
+      skills: [
+        "Ruby on Rails",
+        "HTML",
+        "SCSS",
+        "Stimulus",
+        "Stripe API",
+        "Heroku"
+      ]
+    },
+    {
+      duration: "2021 - Present",
+      title: "Le Wagon - Teacher",
+      description: "Give lectures, support students in challenges, oversee and support development of final projects.",
+      skills: [
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "SQL",
+        "Ruby",
+        "Ruby on Rails",
+        "Templating"
+      ]
+    },
+    {
+      duration: "2022 - Present",
+      title: "Tech4Girls - Volunteer",
+      description: "Teach coding skills to children, help forge partnerships and secure financial support, lead project kick off with a team of UX designers and web developers for a quiz app - advancing our mission to empower girls and underprivileged children in tech.",
+      skills: [
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "Ruby on Rails"
+      ]
+    },
+    {
+      duration: "2021 - 2022",
+      title: "Travel Buddies GmbH - Web Developer",
+      description: "Built and maintained critical components across the whole product portfolio. Work closely with cross-functional teams, including developers, designers, and product managers, to implement and advocate for best practices in web accessibility.",
+      skills: [
+        "HTML",
+        "SCSS",
+        "JavaScript",
+        "PHP",
+        "Wordpress",
+        "Shopify"
+      ]
+    }
+  ];
+</script>
+
+<section class="my-6" id="experience">
+  <h2 class="text-xl/8 font-bold">Experience</h2>
+  <ul>
+    {#each jobs as job, index}
+      <li>
+        <span>{job.duration}</span>
+        <h3>{job.title}</h3>
+        <p>{job.description}</p>
+        <ul>
+          {#each job.skills as skill, i}
+            <li key={i}>{skill}</li>
+          {/each}
+        </ul>
+      </li>
+    {/each}
+  </ul>
+</section>
--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -1,65 +1,16 @@
-<section id="about">
-	<h2>About</h2>
+<script>
+	  import Job from '../components/Job.svelte';
+</script>
+
+<section class="my-6" id="about">
+	<h2 class="text-xl/8 font-bold">About</h2>
 	<p>Dedicated to life long learning. Passionate about fostering environmental and social impact, I actively seek collaborations with brands and projects that align with these values, while also supporting female-founded/led and LGBTQI+ initiatives. Together, we work towards a better future, leveraging my expertise as a Fullstack Developer and active member of the queer and social tech community.</p>
 </section>
-<section id="experience">
-	<h2>Experience</h2>
-	<ul>
-		<li>
-			<span>2022 - Present</span>
-			<h3>Quouch - CTO & Fullstack Developer</h3>
-			<p>Provide leadership in technical decision making. Designed, developed, shipped and now maintain production code for our web application which is now used by 5000+ users worldwide.</p>
-			<ul>
-				<li>Ruby on Rails</li>
-				<li>HTML</li>
-				<li>SCSS</li>
-				<li>Stimulus</li>
-				<li>Stripe API</li>
-				<li>Heroku</li>
-			</ul>
-		</li>
-		<li>
-			<span>2021 - Present</span>
-			<h3>Le Wagon - Teacher</h3>
-			<p>Give lectures, support students in challenges, oversee and support development of final projects.</p>
-			<ul>
-				<li>HTML</li>
-				<li>CSS</li>
-				<li>JavaScript</li>
-				<li>SQL</li>
-				<li>Ruby</li>
-				<li>Ruby on Rails</li>
-				<li>Templating</li>
-			</ul>
-		</li>
-		<li>
-			<span>2022 - Present</span>
-			<h3>Tech4Girls - Volunteer</h3>
-			<p>Teach coding skills to children, help forge partnerships and secure financial support, lead project kick off with a team of UX designers and web developers for a quiz app - advancing our mission to empower girls and underprivileged children in tech.</p>
-			<ul>
-				<li>HTML</li>
-				<li>CSS</li>
-				<li>JavaScript</li>
-				<li>Ruby on Rails</li>
-			</ul>
-		</li>
-		<li>
-			<span>2021 - 2022</span>
-			<h3>Travel Buddies GmbH - Web Developer</h3>
-			<p>Built and maintained critical components across the whole product portfolio. Work closely with cross-functional teams, including developers, designers, and product managers, to implement and advocate for best practices in web accessibility.</p>
-			<ul>
-				<li>HTML</li>
-				<li>SCSS</li>
-				<li>JavaScript</li>
-				<li>PHP</li>
-				<li>Wordpress</li>
-				<li>Shopify</li>
-			</ul>
-		</li>
-	</ul>
-</section>
-<section id="projects">
-	<h2>Projects</h2>
+
+<Job />
+
+<section class="my-6" id="projects">
+	<h2 class="text-xl/8 font-bold">Projects</h2>
 	<ul>
 		<a href="https://quouch-app.com">
 			<li>
@@ -96,8 +47,8 @@
 		</li>
 	</ul>
 </section>
-<section id="yoga">
-	<h2>Yoga</h2>
+<section class="my-6" id="yoga">
+	<h2 class="text-xl/8 font-bold">Yoga</h2>
 	<p>The practice of yoga has been the biggest part of my life besides coding. When I first got into it in 2019, I had to step back from intense exercise and honestly just saw it as a way of movement without stretching my physical abilities. It quickly became an anchor in my life whenever I need grounding. I practice physical yoga on a daily basis, but more importantly do I practice yoga with every breath I take.</p>
 	<p>In February 2024, I completed my 200 hours YTT in Rishikesh, India.</p>
 </section>
