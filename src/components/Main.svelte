9f6c05d0f40139702b0f397211dee1f9204f3977
Lisbeth Purrucker <lisbethpurrucker@gmail.com>
2024-04-17 13:02:42 +0200

    add the section component

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -1,19 +1,9 @@
 <script>
+	  import Section from '../components/Section.svelte';
 	  import Job from '../components/Job.svelte';
 	  import Project from '../components/Project.svelte';
 </script>
 
-<section class="my-6" id="about">
-	<h2 class="text-xl/8 font-bold">About</h2>
-	<p>Dedicated to life long learning. Passionate about fostering environmental and social impact, I actively seek collaborations with brands and projects that align with these values, while also supporting female-founded/led and LGBTQI+ initiatives. Together, we work towards a better future, leveraging my expertise as a Fullstack Developer and active member of the queer and social tech community.</p>
-</section>
-
+<Section />
 <Job />
 <Project />
-
-
-<section class="my-6" id="yoga">
-	<h2 class="text-xl/8 font-bold">Yoga</h2>
-	<p>The practice of yoga has been the biggest part of my life besides coding. When I first got into it in 2019, I had to step back from intense exercise and honestly just saw it as a way of movement without stretching my physical abilities. It quickly became an anchor in my life whenever I need grounding. I practice physical yoga on a daily basis, but more importantly do I practice yoga with every breath I take.</p>
-	<p>In February 2024, I completed my 200 hours YTT in Rishikesh, India.</p>
-</section>
---
+++ src/components/Section.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	interface Section {
+		id: string,
+		title: string;
+		content: string[];
+	}
+
+	let sections:Section[] = [
+		{
+			id: "about",
+			title: "About",
+			content: [
+				"Dedicated to life long learning. Passionate about fostering environmental and social impact, I actively seek collaborations with brands and projects that align with these values, while also supporting female-founded/led and LGBTQI+ initiatives. Together, we work towards a better future, leveraging my expertise as a Fullstack Developer and active member of the queer and social tech community."
+			]
+		},
+		{
+			id: "yoga",
+			title: "Yoga",
+			content: [
+				"The practice of yoga has been the biggest part of my life besides coding. When I first got into it in 2019, I had to step back from intense exercise and honestly just saw it as a way of movement without stretching my physical abilities. It quickly became an anchor in my life whenever I needed grounding. I practice physical yoga, called Asana, on a daily basis, but more importantly do I practice yoga with every breath I take.",
+				"In February 2024, I completed my 200 hours YTT in Rishikesh, India. I am now a certified yoga teacher and am looking forward to sharing my knowledge with others."
+			]
+		}
+	]
+</script>
+
+{#each sections as section}
+	<section class="my-6" id={section.id}>
+		<h2 class="text-xl/8 font-bold">{section.title}</h2>
+		{#each section.content as content}
+			<p>{content}</p>
+		{/each}
+	</section>
+{/each}
